434e75cdc7ed62c1eeef695f72d96b7405c58cda
gaddi22 <gaddiel.morales@gmail.com>
2024-10-30 18:42:57 -0400

    added custom items

--- src/main/java/net/gaddi22/tutorial/TutorialMod.java
+++ src/main/java/net/gaddi22/tutorial/TutorialMod.java
@@ -1,5 +1,6 @@
 package net.gaddi22.tutorial;
 
+import net.gaddi22.tutorial.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -85,6 +86,8 @@ public class TutorialMod {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -108,8 +111,14 @@ public class TutorialMod {
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS)
+        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
             event.accept(EXAMPLE_BLOCK_ITEM);
+        }
+
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.BISMUTH);
+            event.accept(ModItems.RAW_BISUMTH);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
