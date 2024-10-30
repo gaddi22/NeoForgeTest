9fbc6c78fe0fd4d269197fb8f6836c8d44acc429
gaddi22 <gaddiel.morales@gmail.com>
2024-10-30 17:58:07 -0400

    modified package info

--- src/main/java/net/gaddi22/tutorial/TutorialMod.java
+++ src/main/java/net/gaddi22/tutorial/TutorialMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package net.gaddi22.tutorial;
 
 import org.slf4j.Logger;
 
@@ -35,11 +35,10 @@ import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
-{
+@Mod(TutorialMod.MODID)
+public class TutorialMod {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "examplemod";
+    public static final String MODID = "gaddi22neoforgetutorial";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
@@ -69,7 +68,7 @@ public class ExampleMod
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public ExampleMod(IEventBus modEventBus, ModContainer modContainer)
+    public TutorialMod(IEventBus modEventBus, ModContainer modContainer)
     {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
